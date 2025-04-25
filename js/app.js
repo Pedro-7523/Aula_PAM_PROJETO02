79799af0dbe7b6c7529b5eeb372923a5c2d22585
PedroHenrique7523 <140754899+PedroHenrique7523@users.noreply.github.com>
2025-04-25 12:26:43 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var flag = 0;
+
+function muda_cor() {
+
+    let body = document.getElementById("bg");
+
+    if (flag == 0) {
+        body.style.backgroundColor = "#1800cd";
+        flag = 1;
+    } else {
+        body.style.backgroundColor = "#3d828b";
+        flag = 0;
+
+    }
+}
